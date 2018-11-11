a6e17deef0a8d8aca0b7b808b73c56cd2f06a1a2
Kevin Becker <kbecke35@ford.com>
2018-11-11 13:38:03 -0500

    Changes from integration into Arch component

--- cfg/projcfg.h
+++ cfg/projcfg.h
@@ -1,9 +1,9 @@
-/** @file projcfg.h
- *	@brief	One-sentence short description of file.
+/** @file
+ *	@brief	Project Configuration for CWSW Arch Library Unit test environment
  *
  *	Description:
  *
- *	Copyright (c) Kevin L. Becker. All rights reserved.
+ *	Copyright (c) 2019 Kevin L. Becker. All rights reserved.
  *
  *	Original:
  *	Created on: Sep 4, 2016
@@ -39,7 +39,7 @@ extern "C" {
 // ============================================================================
 #define PROJCFG_H__REVSTRING "$Revision: 09042016 $"
 
-
+// ==== PROJECT-INVARIANT CONSTANTS ========================================= {
 /* ==== A WORD ABOUT BUILD TARGETS ============================================
  *	Within Eclipse, the configuration is available as an IDE variable named
  *		"${ConfigName}"
@@ -54,7 +54,7 @@ extern "C" {
  *	Compatibility, for example with MPLAB X.
  *
  *	Within the NetBeans IDE used by MPLAB X, the equivalent mechanism is to
- *	detect off the command-line define "XPRJ_<config>",
+ *	detect off the command-line define "XPRJ_NB_${CONF}",
  *		"#if defined(XPRJ_Debug)"
  *
  *	Note the name of the default configuration created by MPLAB X' New Project
@@ -64,17 +64,23 @@ extern "C" {
  *	Within my development environment, I let Eclipse' New Project Wizard create
  *	its standard Debug and Release configurations, then create new ones for on-
  *	target debugging, as required. I assume my Windows/Linux Debug configuration
- *	is also intended for unit tests, so I rather indiscriminently print things
+ *	is also intended for unit tests, so I rather indiscriminately print things
  *	to the console.
  * ========================================================================= */
 
-/*	==== A FOLLOW-UP WORD ABOUT BUILD TARGETS ==================================
- * ANSI/ISO C says that the preprocesser evaluates and undefined symbol as having
+/* ==== A FOLLOW-UP WORD ABOUT BUILD TARGETS ==================================
+ * ANSI/ISO C says that the preprocessor evaluates an undefined symbol as having
  * the value '0' - however, many of the environments i'm targeting, and also many
- * of the static analysis tools, emit warnings about usage of undefined symbols.
- * in normal C code - and i have a rather strong aversion to using #if defined(x)
- * mechanisms in normal code. therefore, i'll here define all of the non-active
- * build targets.
+ * of the static analysis tools, emit warnings about usage of undefined symbols
+ * in normal C code - and even though i do it when necessary, i have a rather
+ * strong aversion to using #if defined(x) mechanisms in normal code.
+ * therefore, i'll here define all of the non-active build targets.
+ * ========================================================================= */
+
+/* ==== One final word ========================================================
+ * This section is intended to be pristine and unchanged for all projects.
+ * Configuration-specific defines (aka options specific to one of these Build
+ * Targets) should be done in a followup section below.
  * ========================================================================= */
 /* Configuration Check */
 #if defined(XPRJ_Debug)
@@ -90,13 +96,20 @@ extern "C" {
 	/* This configuration is created by Eclipse; we do not want it used */
 	#error For now, do not build with the "Release" build target active
 
+#elif defined(XPRJ_Debug_Win_MinGW)
+	/* This is the configuration intended for development on Windows, using the MinGW tool suite */
+	#define XPRJ_Debug_Linux_GCC		0
+	#define XPRJ_Debug_MSC				0
+	#define	XPRJ_Debug_CVI				0
+	#define XPRJ_Debug_Win_MZ2048EFM	0
+
 #elif defined(XPRJ_Debug_Linux_GCC) || defined(XPRJ_Debug_Linux_GCC_Desktop)
 	/* This is the configuration intended for development & debugging in a Linux VM */
 	/* The 1st is intended to debug on a PowerPC Target from a Linux development environment */
 	/* The 2nd (Desktop) is intended for building within S32DS on Linux for a Linux debugging session */
-	#define	XPRJ_Debug_CVI			0
-	#define	XPRJ_Debug_MSC			0
-	#define	XPRJ_Debug_Win_MinGW 	0
+	#define	XPRJ_Debug_CVI				0
+	#define	XPRJ_Debug_MSC				0
+	#define	XPRJ_Debug_Win_MinGW 		0
 
 #elif defined(XPRJ_Debug_Win_MinGW)
 	/* This is the configuration intended for development on Windows, using the MinGW tool suite */
@@ -118,20 +131,72 @@ extern "C" {
 	/* NOTE: VS8 does not ship w/ headers <stdint.h> or <stdbool.h>, so i found alternate versions
 	 * and copied them into my install directory. i happened to find some web sites w/ versions
 	 * that claimed to work w/ VS8 or VS10, but you could also probably pull them from any other
-	 * compiler you may have installed
+	 * compiler you may have installed.
 	 */
 	#define XPRJ_Debug_Win_MinGW	false
 	#define	XPRJ_Debug_CVI			false
 
-#elif (_CVI_)
-	#define	XPRJ_Debug_CVI			1
-	#define	XPRJ_Debug_MSC			0
-	#define	XPRJ_Debug_Win_MinGW 	0
-	#define XPRJ_Debug_Linux_GCC	0
+#elif defined(_CVI_)
+	#define	XPRJ_Debug_CVI				1
+	#define	XPRJ_Debug_MSC				0
+	#define	XPRJ_Debug_Win_MinGW 		0
+	#define XPRJ_Debug_Linux_GCC		0
+	#define XPRJ_pic32mz_ef_sk			0
+
+#elif (XPRJ_Debug_Win_MZ2048EFM) || defined(XPRJ_pic32mz_ef_sk)
+    /* xMPLAB assigns XPRJ_pic32mz_ef_sk=pic32mz_ef_sk, but the latter signal is not defined to be anything */
+	#define pic32mz_ef_sk				1
+
+	#define Debug_MZ2048EFM				1
+	#define XPRJ_Debug_Win_MinGW		0
+	#define XPRJ_Debug_Linux_GCC		0
+	#define	XPRJ_Debug_CVI				0
+	#define XPRJ_Debug_MSC				0
+
+#elif (XPRJ_Debug_Cx_AtmelSamv71)
+	/* This configuration is intended for the Atmel SAMV71 Xplained Ultra board */
+
+#elif (XPRJ_Debug_XC_MPC57xx_DevKit) || (XPRJ_Debug_XC_MPC57xx_DevKit_RAM)
+	#define pic32mz_ef_sk				(-1)	/* must make this mismatch the project def of the same name */
+
+	/* This configuration is intended for the NXP Calypso Dev Board */
+	#define XPRJ_Debug_Win_MinGW		(0)
+	#define XPRJ_Debug_Linux_GCC		(0)
+	#define	XPRJ_Debug_CVI				(0)
+	#define XPRJ_Debug_MSC				(0)
+	#define Debug_MZ2048EFM				(0)
 
 #else
 #error Must define Eclipse symbol XPRJ_${ConfigName}
 
+#endif
+
+// ====	/PROJECT-INVARIANT CONSTANTS ======================================== }
+
+// ==== PROJECT SPECIFIC CONSTANTS ========================================== {
+// todo: pull in stuff from gen 0 of this same project
+#if (XPRJ_Debug_Linux_GCC)
+	/* enable or disable individual architectural features */
+	#define USE_SYS_CLK					(1)
+	#define USE_SYS_DEVCON				(0)
+	#define USE_SYS_PORTS				(1)
+
+#endif
+
+#if (XPRJ_Debug_CVI)
+	#define pic32mz_ef_sk				(-1)	/* must make this mismatch the project def of the same name */
+	// use cwsw simulated events to achieve separation between UI panels
+	#define USE_NOTIFICATION_EVENTS		(1)
+	#define USE_SYS_CLK					(0)
+	#define USE_SYS_DEVCON				(0)
+	#define USE_SYS_PORTS				(0)
+
+#endif
+
+#if (USE_SYS_DEVCON)
+	#if !(USE_SYS_CLK)
+		#error DEVCON depends on CLK functionality
+	#endif
 #endif
 
 
@@ -157,6 +222,8 @@ extern "C" {
 
 	#endif
 #endif
+
+// ==== /PROJECT SPECIFIC CONSTANTS ========================================= }
 
 
 // ============================================================================
